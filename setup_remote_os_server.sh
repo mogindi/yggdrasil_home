541f5a5ba2496abd6ff18e0fbc054bd146a2127d
Mo G <mo.gindi@gmail.com>
2025-12-20 21:07:58 +0100

    improve remote setup

--- setup_remote_os_server.sh
+++ setup_remote_os_server.sh
@@ -24,10 +24,13 @@ additional_keys_segment="
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ChoqYodykOqMHA6JX4b2B9HZGdbTHpshh3Djt7dYAv1PuVNOaJSm7k65pGppRduvx5BeUhOJV73g9Jar4hHb9sbDYyG2eR0knalOqwfyOOEE2WmVy2FXjxXiC3yPDpPh5JyysQ9Y7PRjYdvjHVlV/aFSqbYrYULZQRw0UkLbObu6y/eUQfg26UmeSYax7CUn4YDWe2Nko+XzlR1gH7Vmj1DzlTMdNdRGAoC0w86OQR4gGQfnDLpoWM8k5rOuNvZHW19hF+d6l0avAfUjoGhConuevjA9KMopLmIEHTnpSXTdOsBeaVpKToIjbToNIgTZTVMOrFv+YbP/2fricElr EXT.mohamed.el.gindi@W-PC2319M5
 "
 
-set -x 
 > ~/.ssh/known_hosts
 ssh-copy-id -o "StrictHostKeyChecking=no" $user@$server
+echo "adding extra keys"
 ssh $user@$server "echo \"$additional_keys_segment\" | tee -a ~/.ssh/authorized_keys"
+echo "adding local key"
+ssh $user@$server "cat ~/.ssh/id_rsa.pub | tee -a ~/.ssh/authorized_keys"
+echo "copying authorized keys to root"
 ssh $user@$server 'sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys'
 
 
