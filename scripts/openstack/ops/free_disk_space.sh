ea8d708d164eb86d2f5f6cc7d7d2b253b15dea1d
Mohamed El Gindi <mo.gindi@gmail.com>
2026-04-08 10:44:11 +0000

    feat(ops): add confirmation prompt to destructive cleaning

--- scripts/openstack/ops/free_disk_space.sh
+++ scripts/openstack/ops/free_disk_space.sh
@@ -1,15 +1,31 @@
-
 #!/bin/bash
 
+
+read -p "WARNING. Destructive cleaning! Continue? (y/N) " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
+
+set -x
 
 # clear journalctl logs
 sudo journalctl --vacuum-size=500M
 
+# clear logs in /var/log
+find /var/log -type f -name "*.log.*" | xargs rm -rf
+
+# clear docker container logs
+find /var/lib/docker/containers -type f -name "*.log" | xargs -I% bash -c '>%'
+
 # clear docker resources
 docker system df -v
-echo RUN `docker system prune -a`
+docker system prune -a --force
 
 # delete old "flog" indices
 curl -X DELETE 'http://10.0.10.100:9200/flog-*?expand_wildcards=all'
 
 # Maybe set index template to make "number_of_replicas": 0 for new flog indices?
+
+
+
