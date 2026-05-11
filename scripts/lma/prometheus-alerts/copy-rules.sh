624846fd13b0329274bb8f2066f11b691de3b465
Mohamed El Gindi <mo.gindi@gmail.com>
2026-05-11 15:31:51 +0200

    fix(lma): fix lma copying rules

--- scripts/lma/prometheus-alerts/copy-rules.sh
+++ scripts/lma/prometheus-alerts/copy-rules.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 dir=$(dirname "$0")
 out_dir="workspace/etc/kolla/config/prometheus"
 
 mkdir -p "$out_dir"
-find "$dir" -iname "*.rules" -print0 | xargs -0 -I % cp % "$out_dir"
+find "$dir" -iname "*.rules" | xargs -I % cp % "$out_dir"
